fa863393db348b28bbfc3b01264fd09754d21258
Curso <Curso@ForGrb-10.ipartekaula.com>
2016-06-24 10:24:52 +0200

    initial import

---
+++ src/com/partek/formacion/AppJava/App.java
@@ -0,0 +1,10 @@
+package com.partek.formacion.AppJava;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Soy una App de Java SE7");
+	}
+
+}
